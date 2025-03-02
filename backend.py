6b5940e234c66b7f3b5cdd089d6b39548a4accc1
sadiarajpoot <sadiakham985@gmail.com>
2025-03-02 23:56:58 +0500

    Fix cohere API usage and update secrets

--- backend.py
+++ backend.py
@@ -8,6 +8,6 @@ co = cohere.Client(os.getenv("COHERE_API_KEY"))
 def get_text_output(input_text):
     response = co.chat(
         model="command-r7b-12-2024",
-        message=input_text
+         query=input_text
     )
     return response.text
